62037fe63f321e935043a2cf4e7c29913240f6e1
Noemiegrau <noemiegrau@outlook.fr>
2021-03-07 17:16:54 -0800

    added length of password code

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,18 +19,20 @@ function generatePassword() {
     passwordLength = parseInt(prompt("How long would you like your password to be? Please enter a number between 8 and 128 characters included."));
   
     if (passwordLength < 8) {
-      window.alert("You choose a password length smaller than 8 characters. Please enter a number between 8 and 128 characters included.");
+      window.alert("You chose a password length smaller than 8 characters. Please enter a number between 8 and 128 characters included.");
 
     } else if (passwordLength > 128) {
-      window.alert("You choose a password length greater than 128 characters. Please enter a number between 8 and 128 characters included.");
+      window.alert("You chose a password length greater than 128 characters. Please enter a number between 8 and 128 characters included.");
 
-    } else (isNaN(passwordLength)) ;{
+    } else if (isNaN(passwordLength)) {
       window.alert("You entered an invalid password length. Please enter a number between 8 and 128 characters included.");
+
+    } else {
+    window.alert("You chose " + passwordLength + " characters for your password.");
     }
   }
   return passwordLength;
-  window.alert()
-};
+}
 
 
 
